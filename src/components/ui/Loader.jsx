8fea3a8443195f05eb8e1d8af517dfca098ef986
Aayash <dangaayash@gmail.com>
2026-04-30 13:11:43 +0545

    Loader

---
+++ src/components/ui/Loader.jsx
@@ -0,0 +1,10 @@
+const Loader = ({ size = 'md' }) => {
+  const sizes = { sm: 'h-5 w-5', md: 'h-10 w-10', lg: 'h-16 w-16' };
+  return (
+    <div className="flex justify-center items-center py-10">
+      <div className={`${sizes[size]} animate-spin rounded-full border-4 border-orange-500 border-t-transparent`} />
+    </div>
+  );
+};
+
+export default Loader;
