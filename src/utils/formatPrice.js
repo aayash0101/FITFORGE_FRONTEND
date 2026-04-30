035aeada5ff3e03b1df0bbaaffe9c119e37a721e
Aayash <dangaayash@gmail.com>
2026-04-30 13:06:24 +0545

    format price

---
+++ src/utils/formatPrice.js
@@ -0,0 +1,8 @@
+// Format number as Nepali Rupees
+export const formatPrice = (price) => {
+  return new Intl.NumberFormat('en-NP', {
+    style: 'currency',
+    currency: 'NPR',
+    minimumFractionDigits: 0,
+  }).format(price);
+};
